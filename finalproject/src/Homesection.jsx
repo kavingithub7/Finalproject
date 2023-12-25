20e927134b970f195aa00be8db49905ac2231243
kavingithub7 <kavink5855@gmail.com>
2023-12-25 12:01:10 +0530

    one section completed

--- finalproject/src/Homesection.jsx
+++ finalproject/src/Homesection.jsx
@@ -6,18 +6,21 @@ export default function Homesection()
                 <div className="forhomeheader">
                     <h1 className="forheaderlineone"> Samsung Galaxy </h1>
                              <h1 className="forheaderlinetwo">  Note 10 + </h1>
-                      
-                   
-                </div>
+                            </div>
                 <div className="forhomecontent">
                     <span className="forcontentline1">Our mission is to help you do the impossible. We’re engineers.</span><br/>
                     <span className="forcontentline2">Inventors. Problem Solvers. What matters to you it also shhould be</span><br/>
                     <span className="forcontentline3">help us, inspires what we do.</span><br/>
                 </div>
-                <div className="forhomebutton"></div>
+                <div className="forhomebutton">
+                    <button className="forbuttonstyle">Explore</button>
+                    </div>
             </div>
             <div className="homesection2">
-                <div className="homesectionimage"></div>
+                <div className="homesectionimage">
+                    
+
+                </div>
             </div>
 
 
