d249cf01e157bb295b563370176471be7c95e395
kavingithub7 <kavink5855@gmail.com>
2023-12-26 21:06:45 +0530

    section two completed

--- finalproject/src/Specificationsection.jsx
+++ finalproject/src/Specificationsection.jsx
@@ -10,12 +10,41 @@ export default function Specificationsection()
             </div>
             <div className="specificationcontent">
                 <div className="features1">
-                    <div className="feature1logo"></div>
-                    <div className="feature1header"></div>
-                    <div className="feature1content"></div>
+                    <div className="feature1logo"><span className='forlogo'><svg xmlns="http://www.w3.org/2000/svg"  width="55" height="65"  fill=" #c11b69" class="bi bi-phone" viewBox="0 0 16 16">
+  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+</svg></span> </div>
+                    <div className="feature1header">
+                        <p>Fully responsive</p>
+                    </div>
+                    <div className="feature1content">
+                        <span className="contenlines">We build pretty complex tools and this</span><br/><span className="contenlines">allows us to take designs and turn them</span> <br/><span className="contenlines">into functional quickly and easily.</span> 
+                    </div>
                 </div>
-                <div className="features2">hii</div>
-                <div className="features3">hii</div>
+                <div className="features2">
+                <div className="feature1logo"><span className='forlogo'><svg xmlns="http://www.w3.org/2000/svg"  width="55" height="65"  fill=" #c11b69" class="bi bi-phone" viewBox="0 0 16 16">
+  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+</svg></span> </div>
+                    <div className="feature1header">
+                        <p>Fully responsive</p>
+                    </div>
+                    <div className="feature1content">
+                        <span className="contenlines">We build pretty complex tools and this</span><br/><span className="contenlines">allows us to take designs and turn them</span> <br/><span className="contenlines">into functional quickly and easily.</span> 
+                    </div>
+                </div>
+                <div className="features3">
+                <div className="feature1logo"><span className='forlogo'><svg xmlns="http://www.w3.org/2000/svg"  width="55" height="65"  fill=" #c11b69" class="bi bi-phone" viewBox="0 0 16 16">
+  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+</svg></span> </div>
+                    <div className="feature1header">
+                        <p>Fully responsive</p>
+                    </div>
+                    <div className="feature1content">
+                        <span className="contenlines">We build pretty complex tools and this</span><br/><span className="contenlines">allows us to take designs and turn them</span> <br/><span className="contenlines">into functional quickly and easily.</span> 
+                    </div>
+                </div>
             </div>
 
         </div>
