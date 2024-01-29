5898564ea8f3687db34a617cfa7f5fc80f690936
kavingithub7 <kavink5855@gmail.com>
2024-01-29 13:34:47 +0530

    Update Multi.jsx

--- finalproject/src/Multi.jsx
+++ finalproject/src/Multi.jsx
@@ -6,17 +6,20 @@ export default function Multi()
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
           items: 5,
-          slidesToSlide: 1 // optional, default to 1.
+          slidesToSlide: 1 ,// optional, default to 1.
+          partialVisibilityGutter: 40 
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
           items: 4,
-          slidesToSlide: 2 // optional, default to 1.
+          slidesToSlide: 2 ,// optional, default to 1.
+          partialVisibilityGutter: 30 
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
           items: 1,
-          slidesToSlide: 1 // optional, default to 1.
+          slidesToSlide: 1, // optional, default to 1
+          partialVisibilityGutter: 30
         }
 
     }
